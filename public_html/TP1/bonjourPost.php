07014e5d2625252f6f44adad1ec0b36967cd7620
yoann LOUAZEL <yoann.louazel@gmail.com>
2019-09-11 17:34:00 +0200

    quasi fin exercice 2

---
+++ public_html/TP1/bonjourPost.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="francais">
+<head>
+    <title>HTML Tutorial</title>
+</head>
+
+<body>
+
+<?php
+if (isset($_POST['module'])) {
+    echo 'Le module s\'appelle ' . htmlspecialchars($_GET['module']) . '.<br>';
+}
+else {
+    echo 'aucun module rempli';
+}?>
+
+</body>
+</html>
