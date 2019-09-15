d208392905d871d3c368c00b7dd9f1ba55a7a6f5
Yoann LOUAZEL <yoann.louazel@gmail.com>
2019-09-15 19:52:51 +0200

    ajout css Exercice 2-3

--- public_html/TP1/Exercice2/listePost.php
+++ public_html/TP1/Exercice2/listePost.php
@@ -2,24 +2,30 @@
 <html lang="francais">
 <head>
     <title>HTML Tutorial</title>
+    <link rel="stylesheet" href="Style/Home.css">
 </head>
 
 <body>
-<?php
-echo $_POST['nbItems'].'<br/>';
-if ( isset($_POST['nbItems'])){
-    $val = (int) htmlentities($_POST['nbItems']);
-    $listeElement = [];
-    for ($i = 0; $i < $_POST['nbItems']; $i++) {
-        $listeElement[] = rand();
-    }
-    echo '<ul>';
-    foreach ($listeElement as $value) {
-        echo '<li>'.$value . '</li>';
-    }
-    echo '</ul>';
-}
-
-?>
+<form id="msform">
+    <fieldset>
+        <h2 class="fs-title">Liste d'items</h2>
+        <?php
+        echo '<h3 class="fs-subtitle">Nombre d\'item : '.$_POST['nbItems'].'</h3>';
+        if ( isset($_POST['nbItems'])){
+            $val = (int) htmlentities($_POST['nbItems']);
+            $listeElement = [];
+            for ($i = 0; $i < $_POST['nbItems']; $i++) {
+                $listeElement[] = rand();
+            }
+            echo '<ul>';
+            foreach ($listeElement as $value) {
+                echo '<li>-  '.$value . '  -</li>';
+            }
+            echo '</ul>';
+        }
+        ?>
+        <a href="http://w31.local/TP1/Exercice2/formulaire.html" name="submit" class="submit action-button">reset</a>
+    </fieldset>
+</form>
 </body>
 </html>
--- public_html/TP1/Exercice3/counter.php
+++ public_html/TP1/Exercice3/counter.php
@@ -4,16 +4,24 @@ session_start();
 $_SESSION['counter']++;
 ?>
 <!DOCTYPE html>
-<html lang="francais">
+<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>HTML Tutorial</title>
+    <meta charset="utf-8" />
+    <title>Counter</title>
+    <link rel="stylesheet" href="Style/Home.css">
 </head>
 
 <body>
+<form id="msform">
+    <!-- fieldsets -->
+    <fieldset>
+        <h2 class="fs-title">Connexion</h2>
 <?php
-echo 'Vous vous êtes connécté ' . $_SESSION['counter'] . ' fois !';
+echo '<h3 class="fs-subtitle">Vous vous êtes connécté ' . $_SESSION['counter'] . ' fois !</h3>';
 ?>
-<a href="http://w31.local/TP1/Exercice3/resetCounter.php">reset</a>
+<a href="http://w31.local/TP1/Exercice3/resetCounter.php" name="submit" class="submit action-button">reset</a>
+    </fieldset>
+</form>
 </body>
 </html>
 
