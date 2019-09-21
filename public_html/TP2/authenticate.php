0f4aef74766f9cdc0320d46505e68d792acb463a
yoann LOUAZEL <ylouazel@unistra.fr>
2019-09-21 16:07:23 +0200

    Exercice 6 fini

--- public_html/TP2/authenticate.php
+++ public_html/TP2/authenticate.php
@@ -4,17 +4,19 @@ if($_SERVER['REQUEST_METHOD'] != 'POST'){
 }
 else{
     include("users.php");
+    session_start();
     if(array_key_exists($_POST['login'],$users)){
         if($users[$_POST['login']]==$_POST['pass']){
-            session_start();
             $_SESSION['login']=$_POST['login'];
             header('Location: http://w31.local/TP2/welcome.php');
         }
         else{
+            $_SESSION['message']='Wrong password';
             header('Location: http://w31.local/TP2/signin.php');
         }
     }
     else{
+        $_SESSION['message']='Wrong login';
         header('Location: http://w31.local/TP2/signin.php');
     }
 }
--- public_html/TP2/signin.php
+++ public_html/TP2/signin.php
@@ -1,13 +1,13 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
-
 <html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="Style/Home.css">
     <title>TP2</title>
 </head>
-
 <body>
     <!-- multistep form -->
     <form id="msform" action="http://w31.local/TP2/authenticate.php" method="post">
@@ -16,9 +16,9 @@
             <h2 class="fs-title">authentification</h2>
             <input type="text" name="login" placeholder="login"/>
             <input type="password" name="pass" placeholder="Password"/>
+            <h4><?php echo $_SESSION['message'];?></h4>
             <input type="submit" name="submit" class="submit action-button" value="Submit"/>
         </fieldset>
     </form>
 </body>
-
 </html>
