6f9ca7ce13d1e869b7b1d5415e54e5413ddf629b
Yoann067 <yoann.louazel@gmail.com>
2019-10-09 16:02:32 +0200

    test bdd

--- public_html/TP3/bdd.php
+++ public_html/TP3/bdd.php
@@ -1,4 +1,5 @@
 <?php
-const SQL_DNS = "mysql:host=2w0vz.myd.infomaniak.com;dbname=2w0vz_2019_w31;";
-const SQL_USERNAME = "admin";
-const SQL_PASSWORD = "adminonestla";
+const SQL_DNS = "mysql:host=localhost;dbname=2019_w31;";
+const SQL_USERNAME = "root";
+const SQL_PASSWORD = "";
+
