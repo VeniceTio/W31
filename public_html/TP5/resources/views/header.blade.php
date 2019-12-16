91909acc043bd8d359fcaabf9949bbb2aea77102
Yoann067 <yoann.louazel@gmail.com>
2019-12-16 08:42:41 +0100

    modif header

--- public_html/TP5/resources/views/header.blade.php
+++ public_html/TP5/resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <header role="banner">
     <a href="/"><img src="/images/LogoMaths.png" width="50px" height="50px" alt="Logo de l'équipe"/></a>
-    <h1>Projet MATHS</h1>
+    <h1>Projet JAC</h1>
     @isset($user)
         <div class="dropdown" role="navigation">
             <a class="dropbtn" href="/admin/welcome"><img src="/images/ImageCompte.png" width="50px" height="50px" aria-label="Mon compte" alt="Logo de l'équipe"/></a>
